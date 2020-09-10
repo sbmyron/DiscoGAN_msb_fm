4284935148196037c179d59e66cf3497739eec67
sbmyron <sbmyron@yahoo.gr>
2020-09-10 15:42:16 +0300

    some other push

--- dataset_creation/scaled2unscaled.py
+++ dataset_creation/scaled2unscaled.py
@@ -30,15 +30,8 @@ for filename in files:
     print(std_real * par / 255, std_imag * par / 255)
     real_m_recon = reconstruct_arrays(real_m_scaled * par, std_real * par)
     imag_m_recon = reconstruct_arrays(imag_m_scaled * par, std_imag * par)
-
-
-    '''
-    stftMat_recon = real_m_recon.astype(float) + 1j * imag_m_recon.astype(float)
-    iStftMat_recon = lc.istft(stftMat_recon, hop_length=_hop_length, center=True)
-    print(iStftMat_recon.shape)
-    '''
+ 
     transparent_img = np.stack((real_m_recon * 255, imag_m_recon * 255, np.zeros(real_m_recon.shape)), axis=-1)
     cv2.imwrite(target_dir+"/"+filename +".png",  transparent_img)
 
-
-    #scipy.io.wavfile.write(recon_files+"/"+"recon"+"_"+filename+".wav", sr, iStftMat_recon)
+ 
